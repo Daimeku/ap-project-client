1f91568a172ce8847cb503c688b5896e2f9eb472
bfranklyn <bfranklyn11@hotmail.com>
2014-04-01 07:02:50 -0800

    NOTE: When you change the login details of this file please don't commit it; use git ignore or the assume unchanged option

--- src/model/DBConnect.java
+++ src/model/DBConnect.java
@@ -18,8 +18,8 @@ import java.sql.SQLException;
 public final class DBConnect {
 	public static final String JDBC_DRIVER = "com.mysql.jdbc.Driver";
 	public static final String DB = "jdbc:mysql://localhost/ap-project";
-	public static final String USER = "Yondaimeku";
-	public static final String PASS = "ashani";
+	public static final String USER = "root";
+	public static final String PASS = "";
 	public Connection conn;
 	public ResultSet result;
 	//public PreparedStatement state = conn.prepareStatement();
